3dc5af270feffc65c620d0eea492944f923000e2
biancaandressa603-code <biancaandressa603@gmail.com>
2026-09-25 23:06:05 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -18,6 +18,8 @@ const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
     }
   });
 }, { threshold: 0.15 });
