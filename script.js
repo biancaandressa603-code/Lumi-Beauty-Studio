871ed1e399d533701aa6da7ce3c94876b5fc070b
biancaandressa603-code <biancaandressa603@gmail.com>
2026-09-25 22:27:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const waButton = document.querySelector('.wa-float');
+const igButton = document.querySelector('.ig-float');
+let scrollTimer;
 
+window.addEventListener('scroll', () => {
+  waButton.classList.add('fading');
+  igButton.classList.add('fading');
+
+  clearTimeout(scrollTimer);
+  scrollTimer = setTimeout(() => {
+    waButton.classList.remove('fading');
+    igButton.classList.remove('fading');
+  }, 400);
+});
